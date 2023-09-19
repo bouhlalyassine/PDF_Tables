666a9fdcf3ce64a6cbe7c3a78cab444ef0fe0660
Bouhlal Yassine <bouhlalyas@gmail.com>
2023-09-19 20:31:36 +0100

    inital commit

---
+++ Extract_PDF_Tables.py
@@ -0,0 +1,120 @@
+import tempfile
+import streamlit as st
+from settings import *
+from streamlit_lottie import st_lottie
+import streamlit as st
+import tabula as tb 
+import pandas as pd
+
+# streamlit run Extract_PDF_Tables.py
+
+st.set_page_config(page_title=TITLE,
+    page_icon=PAGE_ICON,
+    layout="wide")
+
+# --- LOAD CSS, PDF & PROFIL PIC ---
+with open(css_file) as f: # Load the CSS file
+    st.markdown("<style>{}</style>".format(f.read()), unsafe_allow_html=True)
+
+st.markdown("<h2 style=\
+        'text-align : center';\
+        font-weight : bold ;\
+        font-family : Arial;>\
+        Extract Tables from PDF</h2>", unsafe_allow_html=True)
+st.markdown("---")
+
+# st.sidebar.markdown("<br>", unsafe_allow_html=True)
+
+with st.sidebar :
+    clickable_img_logo = get_img_with_href(pp_logo_portfolio, 'https://ybouhlal.streamlit.app/', 70, "blank")
+    st.markdown(clickable_img_logo, unsafe_allow_html=True)
+
+    st.markdown("<br>", unsafe_allow_html=True)
+
+    clickable_img = get_img_with_href(linkpic_code, 'https://github.com/bouhlalyassine/PDF_Tables',
+        170, "blank")
+    st.markdown(clickable_img, unsafe_allow_html=True)
+
+    st.markdown("<br>", unsafe_allow_html=True)
+
+    uploaded_file2 = st.file_uploader("📌 Upload PDF", type=["pdf"], key="fup2", label_visibility="visible")
+
+if uploaded_file2 :
+    try :
+        with st.spinner("Extraction in progress, please wait...") :
+            tables = tb.read_pdf(uploaded_file2, pages = "all")
+
+        if len(tables)==0:
+            #msg_wrn_tbl.empty()
+            st.error("No table detected")
+
+        else :
+            #msg_wrn_tbl.empty()
+            colpi1_t, colpi2_t, colpi3_t = st.columns([42, 16, 42])
+
+            with colpi2_t:
+                st.markdown(f"<h5 style=\
+                'text-align : left';\
+                font-weight : bold ;\
+                font-family : Arial;>\
+                <b>{len(tables)} Tables Detected</h5></b>", unsafe_allow_html=True)
+
+                tabe_idx = st.number_input(label="Label", value=1, min_value=1,
+                    max_value=len(tables), step=1, label_visibility="collapsed")
+
+            temp = tempfile.NamedTemporaryFile(delete=True)
+            temp_filename = temp.name + f'.xlsx'
+
+            with pd.ExcelWriter(temp_filename) as writer:
+                for i in range (len(tables)):
+                    tables[i].to_excel(writer, sheet_name=f"table{i}")
+
+            with open(writer, "rb") as f:
+                binary_data = f.read()
+
+            excel_namefile = f"Tables_{uploaded_file2.name[:-4]}.xlsx"
+
+            st.download_button(
+                label="Download Excel File",
+                data=binary_data,
+                file_name=excel_namefile)
+
+            st.markdown(f"<p style=\
+            'text-align : left';\
+            font-weight : bold ;\
+            font-family : Arial;>\
+            <b><u>Table Displayed :</u> {tabe_idx} / {len(tables)}</p></b>", unsafe_allow_html=True)
+
+            st.dataframe(tables[tabe_idx-1].style.format(na_rep='No Data', precision=1), use_container_width=True)
+    
+    except:
+        st.markdown("<br>", unsafe_allow_html=True)
+        st.error("This tool is not available due to disruptions on one of the\
+            libraries used, a fix is in progress.")
+
+
+else:
+    st.markdown("<br>", unsafe_allow_html=True)
+    colpi_1, colpi_2 = st.columns([85, 15], gap="small")
+    with colpi_1 :
+        st.info("This tool allows you to extract tables from a PDF : \
+            \n ● View them directly on the app\
+            \n ● Save them to your machine in Excel format\
+            \n\n You can upload the PDF to be processed on the left menu")
+
+    with colpi_2:
+        lottie_pdf_table = load_lottiefile(lottie_pdf_table)
+        st_lottie(
+        lottie_pdf_table,
+        speed=1,
+        reverse=False,
+        loop=True,
+        quality="high",
+        height=150)
+    
+    esp_1, col_vid_tuto, esp_2 = st.columns([space, tuto_space, space], gap="small")
+    with col_vid_tuto :
+        with open(tuto_tabl, "rb") as tuto_file:
+            tuto_tabl_byte = tuto_file.read()
+        st.video(tuto_tabl_byte)
+
---
+++ settings.py
@@ -0,0 +1,61 @@
+import json
+from pathlib import Path
+import requests
+import streamlit as st
+import os
+import base64
+
+TITLE = "YB | Porfolio"
+PAGE_ICON ="ico_potfolio.ico"
+
+
+current_dir = Path(__file__).parent if "__file__" in locals() else Path.cwd()
+
+
+# CSS
+css_file = current_dir / "main.css"
+
+
+# My Tutos :
+# size :
+space = 15
+tuto_space = 70
+
+tuto_tabl_p = current_dir / "files" / "tuto_ext_tbl_pdf.mp4"
+tuto_tabl = str(tuto_tabl_p)
+
+pp_logo_portfolio = current_dir / "files" /  "logo_portfolio.png"
+linkpic_code = current_dir / "files" /  "code.png"
+
+
+# lotties :
+lottie_pdf_table = current_dir / "files" / "table_pdf.json"
+
+def load_lottiefile(filepath : str):
+    with open(filepath, "r") as f:
+        return json.load(f)
+
+
+def load_lottieurl(url: str):
+    r = requests.get(url)
+    if r.status_code != 200:
+        return None
+    return r.json()
+
+
+# Clickable img
+@st.cache_data
+def get_base64_of_bin_file(bin_file):
+    with open(bin_file, 'rb') as f:
+        data = f.read()
+    return base64.b64encode(data).decode()
+
+@st.cache_data
+def get_img_with_href(local_img_path, target_url, width, loc):
+    img_format = os.path.splitext(local_img_path)[-1].replace('.', '')
+    bin_str = get_base64_of_bin_file(local_img_path)
+    html_code = f'''
+        <a href="{target_url}" target="_{loc}" style="display: flex; justify-content: center; align-items: center;">
+            <img src="data:image/{img_format};base64,{bin_str}" width="{width}" class="img-hover-effect">
+        </a>'''
+    return html_code
